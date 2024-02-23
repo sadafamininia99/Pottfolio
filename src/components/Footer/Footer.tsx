af6d26bfba75dda1f381c0d8b363a089622992bb
sadaf <aminicomputer96@gmail.com>
2024-02-23 13:56:05 +0330

    fix:remove unnecessary things

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,6 @@ import { Container } from "./styles";
 import reactIcon from "../../assets/react-icon.svg";
 import linkedin from "../../assets/linkedin.svg";
 import githubIcon from "../../assets/github.svg";
-import whatsapp from "../../assets/whatsapp.svg";
 import telegram from "../../assets/telegram.svg";
 import instagramIcon from "../../assets/instagram.svg";
 import discordIcon from "../../assets/discord.png";
