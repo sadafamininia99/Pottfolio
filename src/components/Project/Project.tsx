8d9020598fa2d94c933e9b025330fd70ac8835dd
amininia <aminicomputer96@gmail.com>
2026-09-22 10:14:30 +0330

    *

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -25,6 +25,57 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
+                <a
+                  href="https://sepidsystem.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Speid System Sharif</h3>
+              <p>
+                Sepid System, a knowledge-based company with over a decade of
+                experience, provides comprehensive identity verification
+                solutions for access control and management across various
+                corporate and organizational systems—such as digital
+                authentication, time and attendance tracking, and access
+                management for safe deposit boxes in banks and financial
+                institutions—leveraging advanced biometric technologies
+                including facial, fingerprint, and iris recognition, as well as
+                smart cards.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Typescript</li>
+                <li>Chart.js</li>
+                <li>StoryBook</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
                 <a href="https://webruno.ir/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
